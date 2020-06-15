fd1d86769e991aed942152e28d8da105da2df587
Sebastien Vermandele <sebvermandele@gmail.com>
2020-06-15 13:48:26 -0400

    creation de la table settings et du model setting

--- db/migrate/20200615171941_create_settings.rb
+++ db/migrate/20200615171941_create_settings.rb
@@ -1,5 +1,16 @@
-git statusclass CreateSettings < ActiveRecord::Migration
+class CreateSettings < ActiveRecord::Migration
   def up
+    create_table :settings do |t|
+      t.string :company_name
+      t.text :address
+      t.string :zip_code
+      t.string :city
+      t.string :country
+      t.string :phone_number
+      t.string :email
+      t.float :tps
+      t.float :tvq
+    end
   end
 
   def down
