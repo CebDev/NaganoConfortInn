c30dd62a5812e4eb5ad9366aafe0e3a9487136ef
Sebastien Vermandele <sebvermandele@gmail.com>
2020-06-16 09:28:57 -0400

    ajout validation dans model Setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,14 @@
 class Setting < ActiveRecord::Base
   validates :company_name, presence: true
   validates :address, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, :format => { :with => /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/ ,
+                                                    :message => " is invalid. Please use \"A1A 1AA\" format."}
   validates :city, presence: true
   validates :country, presence: true
-  validates :phone_number, presence: true
-  validates :email, presence: true
-  validates :tps, presence: true
-  validates :tvq, presence: true
+  validates :phone_number, presence: true, :format => { :with => /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/ ,
+                                                        :message => " is invalid."}
+  validates :email, presence: true, :format => { :with => /^(.+)@(.+)$/ ,
+                                                :message => " is invalid."}
+  validates :tps, presence: true, :numericality => true
+  validates :tvq, presence: true, :numericality => true
 end
