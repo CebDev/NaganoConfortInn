77e552478076f55d670d199346203d36bcbf0ae7
Sebastien Vermandele <sebvermandele@gmail.com>
2020-06-18 08:26:06 -0400

    creation form de recherche page accueil

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def index
     @title = 'NaganoConforInn'
+    @setting = Setting.first
+    @room_types = RoomType.all
+    @room_view = RoomView.all
+
   end
 
 
