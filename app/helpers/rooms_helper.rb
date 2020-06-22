933eda5709edd72c427ef5355912c383c9535125
Sebastien Vermandele <sebvermandele@gmail.com>
2020-06-22 19:53:53 -0400

    modification interface room manager et gestion pricing inexistant

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,13 +1,25 @@
 module RoomsHelper
 
-  def set_status_class (room)
+  def set_status_class(room)
+    html = "".html_safe
     if room.status == "Ready"
-      'green'
+      html << "<span class='glyphicon glyphicon-ok-circle green'></span>".html_safe
+      html << "<p class='green'>Ready</p>".html_safe
     elsif room.status == "Busy"
-      'red'
+      html << "<span class='glyphicon glyphicon-remove-circle red'></span>".html_safe
+      html << "<p class='red'>Busy</p>".html_safe
     else
-      'orange'
+      html << "<span class='glyphicon glyphicon-ban-circle orange'></span>".html_safe
+      html << "<p class='orange'>Cleaning</p>".html_safe
     end
+
+  end
+
+  def get_current_room_pricing (room)
+    RoomPricing
+        .where("date_from <= DATE('now') AND date_to >= DATE('now')")
+        .where(room_view_id: room.room_view_id, room_type_id: room.room_type_id, archived_at: nil)
+
   end
 
   def get_room_pricing (room)
